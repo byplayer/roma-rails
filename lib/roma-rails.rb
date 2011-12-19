3782a16215e926ff0a8829af94a7da6e5b3ac074
byplayer <byplayer100@gmail.com>
2011-12-19 17:20:10 +0900

    add version information file

--- lib/roma-rails.rb
+++ lib/roma-rails.rb
@@ -1,3 +1,5 @@
-require File.join(File.dirname(__FILE__),
-                  'rack', 'session', 'roma')
+require File.expand_path(File.join('roma-rails', 'version'),
+                         File.dirname(__FILE__))
+require File.expand_path(File.join('rack', 'session', 'roma'),
+                         File.dirname(__FILE__))
 
---
+++ lib/roma-rails/version.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+module RomaRails
+  # version information.
+  module VERSION
+    # mejor version number
+    MAJOR = 0
+
+    # minor version number
+    MINOR = 0
+
+    # tiny version number
+    TINY = 1
+
+    # version information string
+    STRING = "#{MAJOR}.#{MINOR}.#{TINY}"
+  end
+end
